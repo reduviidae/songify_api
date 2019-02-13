943c2da06dc7298189b02cd9f8b3a12d4fc82009
reduviidae <wiljago@gmail.com>
2019-02-13 17:14:54 -0500

    working on create song function

--- app/controllers/api/v1/songs_controller.rb
+++ app/controllers/api/v1/songs_controller.rb
@@ -3,4 +3,19 @@ class Api::V1::SongsController < ApplicationController
     @songs = Song.all
     render json: @songs
   end
+
+  def create
+  @song = Song.create(song_params)
+  redirect_to `/api/v1/songs/${@song}`
+  end
+
+  def show
+    @song = Song.find(params[:id])
+  end
+
+  private
+  def song_params
+      params.require(:song).permit(:title)
+  end
+
 end
