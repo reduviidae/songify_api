bcf6b0abf6dc32d8a550f206fa9e63bc581e2030
reduviidae <wiljago@gmail.com>
2019-02-13 16:56:55 -0500

    routes changed for cors

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api do
     namespace :v1 do
-      resources :songs, only: [:index, :show, :post]
-      resources :sections, only: [:index, :show, :post]
-      resources :snippets, only: [:index, :show, :post]
+      resources :songs
+      resources :sections
+      resources :snippets
     end
   end
 end
